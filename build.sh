56123f505f258f30509195815efb8377ba8cc859
César Del Solar <delsolar@gmail.com>
2024-12-23 14:07:13 -0500

    add build script that downloads dart-sass

---
+++ build.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# based on https://discourse.gohugo.io/t/using-dart-sass-hugo-and-netlify/37099/7
+# - thanks, @bep!
+
+echo "Install Dart Sass Embedded..."
+
+# This should be in the PATH.
+BIN_DIR=/opt/buildhome/.binrc/bin
+
+DARTSASS_VERSION=1.83.0
+
+mkdir -p $BIN_DIR
+
+curl -LJO https://github.com/sass/dart-sass/releases/download/${DARTSASS_VERSION}/dart-sass-${{DARTSASS_VERSION }}-linux-x64.tar.gz
+
+tar -xvf dart-sass-${DARTSASS_VERSION}-linux-x64.tar.gz;
+
+mv dart-sass $BIN_DIR
+
+rm -rf dart-sass*;
+
+sass --embedded --version
+
+echo "Building..."
+
+hugo --gc --minify
